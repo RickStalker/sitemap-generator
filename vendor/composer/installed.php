cd824634966d6276838ab58bef04a30f73ca2a81
Rick <sopov.rs@gmail.com>
2024-11-04 15:51:01 +0700

    Fix readme

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'rick/sitemap-generator',
-        'pretty_version' => '1.0.0',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0221f1c63e9a2045b280585866fe39fc50756b05',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'rick/sitemap-generator' => array(
-            'pretty_version' => '1.0.0',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0221f1c63e9a2045b280585866fe39fc50756b05',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
